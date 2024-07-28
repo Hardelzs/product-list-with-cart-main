e4c1875224abba8b60ed66e73c4f4e2e188f6e99
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2024-07-28 22:57:10 +0100

    added sweet alert to the project

---
+++ product-list-with-cart-main/js/index.js
@@ -0,0 +1,11 @@
+// Swal.fire("Welcome to Hardelz store!");
+
+const buttons = document.querySelectorAll('#btn1');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    alert("the bullocks");
+  });
+});
+
+

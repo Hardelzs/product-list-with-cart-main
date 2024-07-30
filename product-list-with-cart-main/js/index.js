c207c3845cbddea6ec30b6adff70fba4c5a74dd3
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2024-07-30 08:36:15 +0100

    added display to the confirmation page

--- product-list-with-cart-main/js/index.js
+++ product-list-with-cart-main/js/index.js
@@ -1,14 +1,112 @@
 Swal.fire("Welcome to Hardelz store!");
 
-const buttons = document.querySelectorAll('#btn1');
+const buttons = document.getElementById('btn1');
+const buttons2 = document.getElementById('btn2');
+const buttons3 = document.getElementById('btn3');
+const buttons4 = document.getElementById('btn4');
+const buttons5 = document.getElementById('btn5');
+const buttons6 = document.getElementById('btn6');
+const buttons7 = document.getElementById('btn7');
+const buttons8 = document.getElementById('btn8');
+const buttons9 = document.getElementById('btn9');
+const returntopage = document.getElementById("return");
+const confirmPage = document.getElementById("confirm");
 const countElement = document.getElementById('count');
 let count = 0;
 
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    count++;
-    countElement.textContent = count;
-  });
+
+returntopage.addEventListener("click", () => {
+  returntopage.style.display = 'block'
 });
+
+confirmPage.addEventListener("click", () => {
+  returntopage.style.display = 'none'
+});
+
+
+buttons.addEventListener('click', () => {
+  count++;
+  countElement.textContent = count;
+  const boykill = document.getElementById("boykill")
+  boykill.style.display = 'block'
+  const defaultPage = document.getElementById("default-page")
+  defaultPage.style.opacity = 0 
+});
+
+buttons2.addEventListener('click', () => {
+  count++;
+  countElement.textContent = count;
+  const boykill = document.getElementById("boykill")
+  boykill.style.display = 'block'
+  const defaultPage = document.getElementById("default-page")
+  defaultPage.style.opacity = 0 
+});
+
+buttons3.addEventListener('click', () => {
+  count++;
+  countElement.textContent = count;
+  const boykill = document.getElementById("boykill")
+  boykill.style.display = 'block'
+  const defaultPage = document.getElementById("default-page")
+  defaultPage.style.opacity = 0 
+});
+
+buttons4.addEventListener('click', () => {
+  count++;
+  countElement.textContent = count;
+  const boykill = document.getElementById("boykill")
+  boykill.style.display = 'block'
+  const defaultPage = document.getElementById("default-page")
+  defaultPage.style.opacity = 0 
+});
+
+buttons5.addEventListener('click', () => {
+  count++;
+  countElement.textContent = count;
+  const boykill = document.getElementById("boykill")
+  boykill.style.display = 'block'
+  const defaultPage = document.getElementById("default-page")
+  defaultPage.style.opacity = 0 
+});
+
+buttons6.addEventListener('click', () => {
+  count++;
+  countElement.textContent = count;
+  const boykill = document.getElementById("boykill")
+  boykill.style.display = 'block'
+  const defaultPage = document.getElementById("default-page")
+  defaultPage.style.opacity = 0 
+});
+
+buttons7.addEventListener('click', () => {
+  count++;
+  countElement.textContent = count;
+  const boykill = document.getElementById("boykill")
+  boykill.style.display = 'block'
+  const defaultPage = document.getElementById("default-page")
+  defaultPage.style.opacity = 0 
+});
+
+buttons8.addEventListener('click', () => {
+  count++;
+  countElement.textContent = count;
+  const boykill = document.getElementById("boykill")
+  boykill.style.display = 'block'
+  const defaultPage = document.getElementById("default-page")
+  defaultPage.style.opacity = 0 
+});
+
+buttons9.addEventListener('click', () => {
+  count++;
+  countElement.textContent = count;
+  const boykill = document.getElementById("boykill")
+  boykill.style.display = 'block'
+  const defaultPage = document.getElementById("default-page")
+  defaultPage.style.opacity = 0 
+});
+
+
+
+
 
 
